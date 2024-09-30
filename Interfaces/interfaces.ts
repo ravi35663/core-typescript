69da129dc5729fd38a8026b8e232d46806ed91d9
ravi kumar <ravi.kumar07@telusinternational.com>
2024-09-30 15:40:44 +0530

    learning interfaces

---
+++ Interfaces/interfaces.ts
@@ -0,0 +1,7 @@
+/*
+==> Interfaces and Type Aliases
+        Defining and implementing interfaces
+        Extending interfaces
+        Type aliases and their uses
+        Difference between interfaces and type aliases
+*/
